c092b2ef768f035753e7d20ed33327961a6be274
Garry Mark <94garrymark@gmail.com>
2018-08-26 23:13:09 +0800

    feat(fontawesome): fontawesome dependencies

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from '@/pages/app';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faThumbsUp,
+	faShareSquare,
+	faEye
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add([faThumbsUp, faShareSquare, faEye]);
+
 const Root = () => (
 	<Router>
 		<App />
