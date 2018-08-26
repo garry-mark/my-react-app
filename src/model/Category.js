0ed28a360ca6a8b7c26dab960d053c37661a7854
Garry Mark <94garrymark@gmail.com>
2018-08-26 23:17:36 +0800

    feat(articles item): articles filter UI

---
+++ src/model/Category.js
@@ -0,0 +1,5 @@
+// @flow
+export type Category = {
+	id: number,
+	name: string
+};
--- src/pages/articles/list/list-item/index.js
+++ src/pages/articles/list/list-item/index.js
@@ -1,27 +1,58 @@
 import './style.scss';
 // @flow
 import * as React from 'react';
+import type { Articles } from '@/model/Articles';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-type Props = {};
+type Props = {
+	data: Articles
+};
 
-type State = {};
-
-class ArticlesListItem extends React.Component<Props, State> {
-	static defaultProps: Props = {};
-	state: State = {};
-
+class ArticlesListItem extends React.Component<Props> {
 	constructor(props: Props) {
 		super(props);
 	}
 	componentDidMount() {}
 	render() {
-		const {} = this.props;
-		const {} = this.state;
+		const { data } = this.props;
+		const {
+			id,
+			category,
+			title,
+			content,
+			pageview,
+			like,
+			createTime,
+			updateTime
+		} = data;
 		return (
-			<div>
-				<h3>标题</h3>
-				<p>概要</p>
-				<p>时间</p>
+			<div styleName="articles-list-item">
+				<div styleName="articles-top">
+					<span>
+						来自分类 <em>{category.name}</em>
+					</span>
+					<time>{new Date(createTime).toLocaleString()}</time>
+				</div>
+				<h3>
+					<Link to={`/articles/${id}`}> {title}</Link>
+				</h3>
+				<div styleName="articles-content">{content}</div>
+				<div styleName="articles-bottom">
+					<button>
+						<FontAwesomeIcon icon="thumbs-up" /> 赞 ({like > 999
+							? '999+'
+							: like})
+					</button>
+					<button>
+						<FontAwesomeIcon icon="eye" /> 浏览量（{pageview > 999
+							? '999+'
+							: pageview}）
+					</button>
+					<button>
+						<FontAwesomeIcon icon="share-square" /> 分享
+					</button>
+				</div>
 			</div>
 		);
 	}
