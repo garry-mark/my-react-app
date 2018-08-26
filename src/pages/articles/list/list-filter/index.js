18b2029046e1dbccaa1872fce378b1c2406562d2
Garry Mark <94garrymark@gmail.com>
2018-08-26 23:16:58 +0800

    feat(articles filter): articles filter UI

--- src/pages/articles/list/list-filter/index.js
+++ src/pages/articles/list/list-filter/index.js
@@ -1,26 +1,43 @@
 import './style.scss';
 // @flow
 import * as React from 'react';
+import { ORDER_BY_CREATETIME, ORDER_BY_PAGEVIEW } from '@/const';
 
 type Props = {};
 
-type State = {};
+type State = {
+	activeOrder: string
+};
 
 class ArticlesListFilter extends React.Component<Props, State> {
 	static defaultProps: Props = {};
-	state: State = {};
+	state: State = {
+		activeOrder: ORDER_BY_CREATETIME
+	};
 
 	constructor(props: Props) {
 		super(props);
 	}
 	componentDidMount() {}
+	changeListOrder = newOrder => {
+		this.setState({ activeOrder: newOrder });
+	};
+
 	render() {
 		const {} = this.props;
-		const {} = this.state;
+		const { activeOrder } = this.state;
 		return (
-			<div>
-				<button>最新</button>
-				<button>最Top</button>
+			<div styleName="articles-list-filter">
+				<button
+					styleName={activeOrder === ORDER_BY_CREATETIME ? 'avtive' : ''}
+					onClick={e => this.changeListOrder(ORDER_BY_CREATETIME, e)}>
+					最新
+				</button>
+				<button
+					styleName={activeOrder === ORDER_BY_PAGEVIEW ? 'avtive' : ''}
+					onClick={e => this.changeListOrder(ORDER_BY_PAGEVIEW, e)}>
+					最Top
+				</button>
 			</div>
 		);
 	}
