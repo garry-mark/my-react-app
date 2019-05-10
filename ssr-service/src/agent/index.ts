e13817b92830b1dd59fccaf6b3d8fb3c0cfd6103
Garry Mark <94garrymark@gmail.com>
2019-05-11 01:07:31 +0800

    fix(ssr agent): add browser baseURL

--- ssr-service/src/agent/index.ts
+++ ssr-service/src/agent/index.ts
@@ -2,24 +2,25 @@ import browserAgent from './browser-agent';
 import serverAgent from './server-agent';
 
 const isProd = process.env.NODE_ENV === 'production';
-const baseURL = isProd ? 'http://bussiness-service:5000/api' : 'http://localhost:5000/api';
+const browserBaseURL = isProd ? 'http://120.78.130.154:5000/api' : 'http://localhost:5000/api';
+const serverBaseURL = isProd ? 'http://bussiness-service:5000/api' : 'http://localhost:5000/api';
 
 export function getAgent() {
   try {
     if (window) {
       return browserAgent({
         // only this way to request,bc it run in browser
-        baseURL: 'http://localhost:5000/api',
+        baseURL: browserBaseURL,
         timeout: 1000 * 10
       });
     } else {
       return serverAgent({
-        baseURL
+        baseURL: serverBaseURL
       });
     }
   } catch (e) {
     return serverAgent({
-      baseURL
+      baseURL: serverBaseURL
     });
   }
 }
